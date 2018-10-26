3f26f1bc1e1f25c40590941e60942b2676b81748
yasvanth <byasvanth@live.in>
2018-10-26 12:19:40 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,9 +4,4 @@ node default{
     content => 'This is a readme',
     owner   => 'root',
   }
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a readme',
-    owner   => 'root',
-  }
 }
